ae6a7abf38f44424caa54d047f585bc25fcb92b7
Vikas <vikas.02k02@gmail.com>
2023-12-28 17:55:49 +0530

    AdminsignUp

---
+++ app/src/main/java/com/axciom/Admin/adminSignup.java
@@ -0,0 +1,16 @@
+package com.axciom.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.axciom.R;
+
+public class adminSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_signup);
+    }
+}
